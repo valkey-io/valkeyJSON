09612c9ba5d9be2ae658ca6ec283e17ac539a7f6
Joe Hu <joehu888@gmail.com>
2024-12-13 19:51:13 -0500

    Remove RDB compatibility test and data (#27)

--- tst/integration/test_rdb.py
+++ tst/integration/test_rdb.py
@@ -34,31 +34,3 @@ class TestRdb(JsonTestCase):
         assert True == client.execute_command('save')
         client.execute_command('FLUSHDB')
         assert b'OK' == client.execute_command('DEBUG', 'RELOAD', 'NOSAVE')
-
-    def test_import_rejson_rdbs(self):
-        '''
-        Verify we can load RDBs generated from ReJSON.
-        Each RDB file contains JSON key "store" (data/store.json).
-        '''
-        self.load_rdbs_from_dir('rdb_rejson')
-
-    def load_rdbs_from_dir(self, dir):
-        src_dir = os.getenv('SOURCE_DIR')
-        rdb_dir = f"{src_dir}/tst/integration/{dir}"
-        assert os.path.exists(rdb_dir)
-        for (dirpath, dirnames, filenames) in os.walk(rdb_dir):
-            for filename in filenames:
-                if pathlib.Path(filename).suffix == '.rdb':
-                    file_path = os.path.join(dirpath, filename)
-                    self.load_rdb_file(file_path, filename)
-
-    def load_rdb_file(self, rdb_path, rdb_name):
-        new_path = os.path.join(self.testdir, rdb_name)
-        os.system(f"cp -f {rdb_path} {new_path}")
-        logging.info(f"Loading RDB file {new_path}")
-        self.client.execute_command(f"config set dbfilename {rdb_name}")
-        self.client.execute_command("debug reload nosave")
-        self.verify_keys_in_rejson_rdb()
-
-    def verify_keys_in_rejson_rdb(self):
-        assert b'["The World Almanac and Book of Facts 2021"]' == self.client.execute_command('json.get', 'store', '$..books[?(@.price>10&&@.price<22&&@.isbn)].title')
